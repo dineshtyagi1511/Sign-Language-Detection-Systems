dd6a47ec8939e357cb0166e13dc915f792b9edde
dineshtyagi1511 <dineshtyagi15112002@gmail.com>
2026-03-13 11:23:38 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup , find_packages
+
+setup(
+    name = "sign-language-recognition",
+    version="0.1",
+    author = "Dinesh",
+    packages=find_packages(),
+    author_email="dineshtyagi1511@gmail.com",
+    install_requires=[]
+)
